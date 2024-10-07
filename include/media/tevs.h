7ee43aa2838bddc37ae20ec1950f8f0e04579504
Jerry Liu <jerry.liu@technexion.com>
2024-10-07 14:05:14 +0800

    modified struct tevs_platform_data to fix bugs of gpios

--- include/media/tevs.h
+++ include/media/tevs.h
@@ -9,6 +9,9 @@ struct tevs_platform_data {
 	unsigned int port;
 	unsigned int lanes;
 	uint32_t i2c_slave_address;
+	int irq_pin;
+	unsigned int irq_pin_flags;
+	char irq_pin_name[16];
 	int reset_pin;
 	int standby_pin;
 	char suffix;
